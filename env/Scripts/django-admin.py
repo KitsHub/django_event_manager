d04564816c60837f271a24e379534ee4326d2070
tripathi kirtan <tripathikirtan9@gmail.com>
2024-06-29 17:46:45 +0530

    odoo submission

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\kirtan tripathi\Downloads\PDPU\odoo\event orchastration\django_event_management\django-event-management\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
